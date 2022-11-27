6d97a1dd3fe7fcc06ea2d4c26a0dab47f97d7a68
caohu <673039293@qq.com>
2022-11-28 00:29:43 +0800

    新建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>&nbsp;&nbsp;哈喽，这是马老师的小网站(三方链接)</h1>
+
+    <div class="btn">登录融易通</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.btn{
+  background: blue;
+  font-size: 20px;
+  color: #fff;
+  text-align: center;
+  padding: 15px 0;
+  border-radius: 7px;
+  margin-top: 200px;
+}
+</style>
